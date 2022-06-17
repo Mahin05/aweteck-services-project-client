8127881d1d0d74f08c2b54178c16381a2ca2244b
Mahin05 <mahin.islam05@gmail.com>
2022-06-17 13:29:08 +0600

    updated code

--- src/Pages/Home/IndividualUserDetailModa/IndividualUserDetailModa.js
+++ src/Pages/Home/IndividualUserDetailModa/IndividualUserDetailModa.js
@@ -11,7 +11,7 @@ const IndividualUserDetailModa = ({detail}) => {
                     <label for="user-info-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h2 class="card-title text-4xl name">Username: <span className='text-style'>{username}</span></h2>
                     <p className="steps"><span className='title'>Name: <span className='text-style'>{name}</span></span></p>
-                    <p className="steps"><span className='title'>Email: <span className='text-style'>{email}</span></span></p>
+                    <p className="steps"><span className='title'>Email: <span className='text-style'>{email}</span></span></p> <br />
                     <p className="steps"><span className='title'>Address:</span></p>
                     <li className='title-two'>City: <span className='text-style'>{address?.city}</span></li>
                     <li className='title-two'>Street: <span className='text-style'>{address?.street}</span></li>
